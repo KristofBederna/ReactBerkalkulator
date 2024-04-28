ec74c56792ae8294bba3de965190a4a67a749ad5
KristofBederna <bederna.kristof@gmail.com>
2024-04-28 14:36:39 +0200

    final vol2

--- src/components/FamilyMemberTabs/FamilyMemberTabs.jsx
+++ src/components/FamilyMemberTabs/FamilyMemberTabs.jsx
@@ -9,7 +9,6 @@ const FamilyMemberTabs = ({ users, setUsers, setCurrentUser }) => {
       this.netIncome = 0;
       this.sliderPercentage = 0;
       this.inputValue = 0;
-      this.taxes = 0;
       this.under25Checked = false;
       this.personalTaxCutChecked = false;
       this.recentlyMarriedChecked = false;
